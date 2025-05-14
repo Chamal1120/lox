68cd8e857828ef8d70c5c0def1c5d670b17c0a10
Chamal1120 <kenway213beep@gmail.com>
2025-05-15 00:21:27 +0530

    add number lexmes

--- com/craftinginterpreters/lox/scanner.java
+++ com/craftinginterpreters/lox/scanner.java
@@ -70,11 +70,28 @@ class Scanner {
         break;
       case '"': string(); break;
       default:
-        Lox.error(line, "Unexpected character.");
+        if (isDigit(c)) {
+          number();
+        } else {
+          Lox.error(line, "Unexpected character.");
+        }
+        break;
     }
   }
   
   //******************* Helper functions **************************************
+
+  private void number() {
+    while (isDigit(peek())) advance();
+
+    // Look for a fractional part
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "." and advance current
+      advance();
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
 
   private void string() {
     while (peek() != '"' && !isAtEnd()) {
@@ -111,6 +128,15 @@ class Scanner {
   private Char peek() {
     if (isAtEnd()) return '\0';
     return source.charAt(current);
+  }
+
+  private Char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c<= '9';
   }
 
   // Check if the token has scanned to the end
